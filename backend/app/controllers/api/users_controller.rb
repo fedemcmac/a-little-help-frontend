3e063173546049e7aea604f3b5fbab5b3d1f1d4d
Federica Macchi <federica.macchi3@gmail.com>
2019-08-28 21:37:04 +0100

    auth backend done

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -10,8 +10,8 @@ class Api::UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
-            # render json: { user: UserSerializer.new(user)}, status: :created
-            render json: { user: UserSerializer.new(user)}, status: :created
+            # render json: { user: UserSerializer.new(user), token: user.token }, status: :created
+            render json: { user: UserSerializer.new(user), token: encode_token({user_id: user.id}) }, status: :created
         else
             render json: { errors: user.errors.full_messages }, status: :not_accepted
         end
