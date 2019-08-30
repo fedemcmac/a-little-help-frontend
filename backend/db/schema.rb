ebb4d39f73097f2847304bf2eb804ff382e273d4
Federica Macchi <federica.macchi3@gmail.com>
2019-08-30 20:24:43 +0100

    added instructions to user table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20190826102312) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.boolean "instructions", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
