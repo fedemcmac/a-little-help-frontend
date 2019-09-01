889195d80620316125e52634b444a4a03649c5de
Federica Macchi <federica.macchi3@gmail.com>
2019-09-01 23:16:23 +0100

    user can drop tasks and dom is updated

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::UsersController < ApplicationController
 
     # skip_before_action :set_current_user, only: [:create]
-    
+     
     def index
         users = User.all
         render json: users
@@ -22,10 +22,18 @@ class Api::UsersController < ApplicationController
         render json: { user: FullUserSerializer.new(user) }
     end 
     
-    # def add_job
+    def drop_job
+        user_job = UserJob.find_by(job_id: params[:job_id], user_id: @current_user.id )
+        user_job.destroy
+        
+
+        # render json: { job: JobSerializer.new(job) }
+
+        # @cocktail.destroy
+
     #     user_job = UserJob.create(job_id: params[:job_id], user_id: @current_user.id )
     #     render json: { user: FullUserSerializer.new(@current_user) }
-    # end
+    end
 
     def update
         user = User.find(params[:id])
